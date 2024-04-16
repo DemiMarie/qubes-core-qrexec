c223223186a876091fdd60fcb8fae7dd602ebaf6
Demi Marie Obenour <demi@invisiblethingslab.com>
2024-04-16 19:47:44 -0400

    Strip "QUBESRPC " prefix from service call commands
    It carries no information, and various parts of the code must strip it.
    Just omit it from the command entirely.  Whether a command is an RPC
    command should be determined by the service descriptor being non-NULL.
    
    Review with "git diff --ignore-space-change".

--- daemon/qrexec-client.c
+++ daemon/qrexec-client.c
@@ -66,11 +66,13 @@ static _Noreturn void do_exec(const char *prog,
                               const char *cmdline,
                               const char *username __attribute__((unused)))
 {
-    /* Avoid calling RPC command through shell.
-     * Qrexec-client is always in a login session. */
-    exec_qubes_rpc_if_requested2(prog, cmdline, environ, false);
+    /* avoid calling RPC service through shell */
+    if (prog) {
+        /* qrexec-client is always in a login session. */
+        exec_qubes_rpc2(prog, cmdline, environ, false);
+    }
 
-    /* if above haven't executed RPC command, pass it to shell */
+    /* if above haven't executed RPC service, pass it to shell */
     execl("/bin/bash", "bash", "-c", cmdline, NULL);
     PERROR("exec bash");
     exit(1);
